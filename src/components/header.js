66a19dca311e7ad98024b545eaf1dfc315174273
tricu10 <tricu10@cs.students.ubc.ca>
2021-04-02 14:30:09 -0700

    first version, added header, patientinfo

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
-import {Link} from 'react-scroll'
+import React, { Component } from "react";
+import { Link } from "react-scroll";
 
 export default class header extends Component {
     render() {
         return (
-            <ul style={{display: 'flex', listStyle: 'none', justifyContent: 'space-around'}}>
-          <li><Link activeClass="active" to="home" spy={true} smooth={true}>Welcome</Link></li>
-          <li><Link  to="about" spy={true} smooth={true}>Diagnosis</Link></li>
-          <li><Link  to="contact" spy={true} smooth={true}>Medication</Link></li>
-          <li><Link  to="service" spy={true} smooth={true}>Prescription</Link></li>
+            <ul
+                style={{
+                    display: "flex",
+                    listStyle: "none",
+                    justifyContent: "space-around",
+                }}
+            >
+                <li>
+                    <Link
+                        activeClass="active"
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                    >
+                        Welcome
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" spy={true} smooth={true}>
+                        Diagnosis
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" spy={true} smooth={true}>
+                        Medication
+                    </Link>
+                </li>
+                <li>
+                    <Link to="service" spy={true} smooth={true}>
+                        Prescription
+                    </Link>
+                </li>
             </ul>
-        )
+        );
     }
 }

a2770be96cd76e69229c478d18bb858e80d22fee
Nicole Lee <nicolelee@Nicoles-MacBook-Pro.local>
2021-04-04 20:38:13 -0700

    commiting the new changes

---
+++ src/components/Medication/Medication.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Wrapper from '../Wrapper/Wrapper';
+import styled from 'styled-components';
+
+const MedicationStyled = styled.div`background-color: white;`;
+
+
+export default class Medication extends Component {
+    render() {
+        return (
+            <MedicationStyled id="medication">
+                 <Wrapper> 
+                 <div class="container">
+                 <h1>This is Medication section</h1>
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                repellendus. Totam nihil similique a repellat minus dolor amet
+                quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime
+                minima animi.
+                </div>
+            </Wrapper>
+           
+            </MedicationStyled>
+        );
+    }
+}

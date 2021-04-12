7db22feef2d5c57cd1a7b71e6d9398a3b2a9a666
Nicole Lee <nicolelee@Nicoles-MacBook-Pro.local>
2021-04-12 14:22:14 -0700

    cleaned up code

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const WrapperStyled = styled.div`
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    h1 {
-        font-size: 45px;
-    }
-    p {
-        font-size: 22px;
-    }
-    margin-left: 200px;
-    padding: 20px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: 45px;
+  }
+  p {
+    font-size: 22px;
+  }
+  margin-left: 200px;
+  padding: 20px;
 `;
 
 export default function Wrapper({ children }) {
-    return <WrapperStyled>{children}</WrapperStyled>;
+  return <WrapperStyled>{children}</WrapperStyled>;
 }

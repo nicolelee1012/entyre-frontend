8c6d0ddd8f5d87bf3e75ebab65cae386b7a0b730
Nicole Lee <nicolelee@Nicoles-MacBook-Pro.local>
2021-04-27 10:48:20 -0700

    diagnosis autocomplete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Diagnosis from "./components/Diagnosis/Diagnosis";
 import SideEffects from "./components/SideEffects/SideEffects";
 import Optimization from "./components/Optimization/Optimization";
 import PatientReport from "./components/PatientReport/PatientReport";
-<<<<<<< HEAD
 import Logo from "./components/Logo/Logo";
 
 function App() {
@@ -22,22 +21,6 @@ function App() {
       <PatientReport />
     </div>
   );
-=======
-
-function App() {
-    return (
-        <div className="App">
-            <Logo />
-            <Header />
-            <Home />
-            <PatientInfo />
-            <Diagnosis />
-            <SideEffects />
-           <Optimization />
-            <PatientReport />
-        </div>
-    );
->>>>>>> 2876a6026ead314f55ec25233dff5d030adfe16c
 }
 
 export default App;

59b50109710d82f56d0df632b2f19e8d8327d752
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2021-05-02 14:58:06 -0400

    Restore for two separate ports

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 //export const BASE_API_URL = 'http://localhost:8080';
-export const BASE_API_URL = '';
+//export const BASE_API_URL = '';
+export const BASE_API_URL = 'https://entyre-backend.herokuapp.com';
